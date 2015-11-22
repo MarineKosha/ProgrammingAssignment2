0ce13f82da4eac1368d53ddea6a54850b57d981e
Marine <impavlovich@gmail.com>
2015-11-22 20:46:53 +0200

    Edit cachematrix.R
    Edited by placing my solution.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix creates [invertible] matrix object that can cache its inverse. 
+## cacheSolve computes the inverse of the matrix object returned by makeCacheMatrix
+## or gets the inverse from the cache if it has been calculated already.
 
-## Write a short comment describing this function
+## makeCacheMatrix stores a list of 4 functions: set, get, setInv, getInv
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) { ## changes initial matrix object x with object y
+                x <<- y
+                inv <<- NULL ## restores to null the previous inverse if "set" were applied 
+        }
+        get <- function() x ## returns initial matrix object x or the result of "set"
+        setInv <- function() inv <<- solve(x) ## stores the result of the inverse calculation in inv variable
+        getInv <- function() inv ## returns the above result 
+        list(set = set, get = get, setInv = setInv, getInv = getInv)
 }
 
+## cacheSolve retrieves the inverse of the matrix object returned by makeCacheMatrix
+## if the inverse already exists, otherwise calculates it.
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x = matrix(), ...) {
+        inv <- x$getInv() ## verifies the value inv exists in memory and is not NULL 
+        if (!is.null(inv)) { ## if it does and not NULL
+                message("getting cached data") ## then returns a message
+                return(inv) ## and the inverse itself
+        }
+        getdata <- x$get() ## if inv doesn't exist then gets the matrix stored with makeCacheMatrix
+        inv <- solve(getdata, ...) ## calculates the inverse
+        x$setInv(inv) ## stores it in the object generated assigned with makeCacheMatrix
+        inv ## and returns it
 }
